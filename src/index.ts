f224ab42f6bf0609aa4d606830aa593382192878
satchelspencer <satchelspencer@gmail.com>
2024-12-09 00:41:47 -0700

    plugin helper setup

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { CardState, InitOptions, KeyEventMessage, ReadyMessage } from './types'
+
+export { CardState } from './types'
+
+export function hsrsPlugin(
+  options: InitOptions,
+  onState: (state: CardState) => void
+): () => void {
+  const { allowedOrigins } = options,
+    handleMessage = (event: MessageEvent) => {
+      if (allowedOrigins && !allowedOrigins.includes(event.origin)) return
+
+      const data = event.data
+      if (typeof data === 'object' && data?.type === 'state') {
+        onState(data.state)
+      }
+    },
+    handleKeyDown = (event: KeyboardEvent) => {
+      const msg: KeyEventMessage = {
+        type: 'key',
+        key: event.key,
+        meta: event.metaKey,
+      }
+
+      const targetOrigin = allowedOrigins?.[0] || '*'
+      window.parent.postMessage(msg, targetOrigin)
+    }
+
+  window.addEventListener('message', handleMessage)
+  window.addEventListener('keydown', handleKeyDown)
+
+  const msg: ReadyMessage = { type: 'ready' },
+    targetOrigin = allowedOrigins?.[0] || '*'
+  window.parent.postMessage(msg, targetOrigin)
+
+  return () => {
+    window.removeEventListener('message', handleMessage)
+    window.removeEventListener('keydown', handleKeyDown)
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export interface CardState {
+  value: { [prop: string]: string }
+  vars: { [prop: string]: string }
+  revealed: boolean
+  property: string
+}
+
+export interface StateUpdateMessage {
+  type: 'state'
+  state: CardState
+}
+
+export interface KeyEventMessage {
+  type: 'key'
+  key: string
+  meta: boolean
+}
+
+export interface ReadyMessage {
+  type: 'ready'
+}
+
+export interface InitOptions {
+  allowedOrigins?: string[]
+}
+
+export type IncomingMessage = StateUpdateMessage
+
+export type OutgoingMessage = ReadyMessage | KeyEventMessage
